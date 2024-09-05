14f93aca01d3117d038dfb5b6be77856de84f6ea
André Rosatto <linus1178@gmail.com>
2024-09-05 14:15:59 -0300

    mudança dos dados de conexão do MySQL para o arquivo .env

--- config/connect.php
+++ config/connect.php
@@ -1,12 +1,9 @@
 <?php
-	define('HOST', 'localhost');
-	define('DATABASE', 'newtab_php');
-	define('USERNAME', 'root');
-	define('PASSWORD', 'root');
-	
-	try {
-		$conn = new PDO('mysql:host=' . HOST . ';dbname=' . DATABASE . ';charset=utf8', USERNAME, PASSWORD);
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	} catch (PDOException $e) {
-		echo "connection error: " . $e->getMessage();
-	}
+include '.env';
+
+try {
+	$conn = new PDO('mysql:host=' . HOST . ';dbname=' . DATABASE . ';charset=utf8', USERNAME, PASSWORD);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	echo "connection error: " . $e->getMessage();
+}
